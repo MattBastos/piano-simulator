08899048c3c0556945a8af0f9898ead342ef7074
Matthews Bastos <matthewsbasttos@gmail.com>
2023-11-01 20:53:03 -0300

    style: Add pression effect to piano keys when user press keyboard button

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -5,6 +5,13 @@ let audio = new Audio("src/tunes/src_tunes_y.wav");
 const playTune = (key) => {
   audio.src = `src/tunes/${key}.wav`;
   audio.play();
+
+  const clickedKey = document.querySelector(`[data-key="${key}"]`);
+  clickedKey.classList.add("active");
+
+  setTimeout(() => {
+    clickedKey.classList.remove("active");
+  }, 150);
 };
 
 pianoKeys.forEach((key) =>
